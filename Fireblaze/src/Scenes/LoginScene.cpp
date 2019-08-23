f877e166dbba0ff70f504cb817e4718dde11bea3
Jordan Morrison <jordan.thomas.morrison@gmail.com>
2019-08-23 21:02:37 +0800

    Updated UITextElement

--- Fireblaze/src/Scenes/LoginScene.cpp
+++ Fireblaze/src/Scenes/LoginScene.cpp
@@ -12,7 +12,7 @@ namespace Fireblaze
 
 		Vec3Node& coords = graph.AddNode<Vec3Node>();
 		coords.SetXY(graph.GetContext().VertexTexCoord().GetValue());
-		coords.SetZ(graph.AddNode(std::make_unique<FloatNode>(1.0f)).GetValue());
+		coords.SetZ(graph.AddNode(std::make_unique<ConstantFloatNode>(1.0f)).GetValue());
 		MultiplyNode& mul = graph.AddNode<MultiplyNode>();
 		mul.SetInputA(texCoordTransform.GetValue());
 		mul.SetInputB(coords.GetValue());
@@ -31,11 +31,14 @@ namespace Fireblaze
 			{
 				currentTime += Time::Get().RenderingTimeline().DeltaTime();
 				
-				float speed = 0.5f;
-				int row = (int)(currentTime / (speed)) % 8;
-				int col = (int)(currentTime / (speed / 8.0f)) % 8;
-				Matrix3f result = Matrix3f::Translation(row / 8.0f, col / 8.0f, 0.0f);
-				result = result * Matrix3f::Scale(1.0f / 8.0f, 1.0f / 8.0f, 1.0f);
+				int maxRows = 8;
+				int maxCols = 8;
+
+				float speed = 0.3f;
+				int row = (maxRows - 1) - (int)(currentTime / (speed)) % maxRows;
+				int col = (int)(currentTime / (speed / (float)maxCols)) % maxCols;
+				Matrix3f result = Matrix3f::Translation(col / (float)maxCols, row / (float)maxRows, 0.0f);
+				result = result * Matrix3f::Scale(1.0f / (float)maxCols, 1.0f / (float)maxRows, 1.0f);
 				return result;
 			});
 
@@ -45,30 +48,31 @@ namespace Fireblaze
 	void CreateLoginScene(Scene& scene, int width, int height, Scene& registerScene)
 	{
 		Camera* loginCamera = scene.CreateCamera(Projection::Orthographic(0, width, 0, height, -100, 100));
-		Layer& loginLayer = scene.CreateLayer(loginCamera);
+		Layer& loginLayer = scene.CreateLayer(loginCamera, 100);
 
-		UIsurface& background = loginLayer.UI().Rectangle(width, height, Color::Black, Transform({ width / 2.0f, height / 2.0f, -90 }));
+		UISurface& background = loginLayer.UI().Root().CreateSurface(width, height, Color::Black, Transform({ width / 2.0f, height / 2.0f, -90 }));
 
 		ResourceManager::Get().LoadPack("res/resources.pack", [&background](const ResourcePack& pack)
 			{
 				ResourceExtractor resources(pack);
-				background.Object()->mesh().Mesh.Materials[0] = CreateBackgroundMaterial(resources.GetResourcePtr<Texture2D>("FireTitle8x8"));
+				background.GetGameObject()->mesh().Mesh.Materials[0] = CreateBackgroundMaterial(resources.GetResourcePtr<Texture2D>("FireTitle8x8"));
 			});
 
-		UIsurface& loginBackdrop = background.Rectangle(400, 600, Color(200, 190, 200), Transform({ 0, 0, 1 }));
-		Text& title = loginBackdrop.Text("Fireblaze", ResourceManager::Get().Fonts().Verdana(48), Color::Black, Transform({ 0, 200, 1 }), AlignH::Center);
+		UISurface& loginBackdrop = background.CreateSurface(400, 600, Color(200, 190, 200, 200), Transform({ 0, 0, 1 }));
+		UIText& title = loginBackdrop.CreateText("Fireblaze", ResourceManager::Get().Fonts().Verdana(48), Color::Black, Transform({ 0, 200, 1 }), AlignH::Center);
 
-		UIsurface& usernameBox = loginBackdrop.Rectangle(300, 50, Color(100, 100, 100), Transform({ 0, 50, 1 }));
-		Text& usernameText = usernameBox.Text("Username:", ResourceManager::Get().Fonts().Calibri(16), Color::Black, Transform({ -150, 32, 0 }), AlignH::Left, AlignV::Bottom);
-		UIsurface& passwordBox = usernameBox.Rectangle(300, 50, Color(100, 100, 100), Transform({ 0, -100, 0 }));
-		Text& passwordText = passwordBox.Text("Password:", ResourceManager::Get().Fonts().Calibri(16), Color::Black, Transform({ -150, 32, 0 }), AlignH::Left, AlignV::Bottom);
+		UITextInput& usernameBox = loginBackdrop.CreateTextInput(300, 50, Color(100, 100, 100), ResourceManager::Get().Fonts().Calibri(20), Color::Black, Transform({ 0, 50, 1 }));
+		UIText& usernameText = usernameBox.CreateText("Username:", ResourceManager::Get().Fonts().Calibri(16), Color::Black, Transform({ -150, 32, 0 }), AlignH::Left, AlignV::Bottom);
+		UITextInput& passwordBox = usernameBox.CreateTextInput(300, 50, Color(100, 100, 100), ResourceManager::Get().Fonts().Calibri(20), Color::Black, Transform({ 0, -100, 0 }));
+		UIText& passwordText = passwordBox.CreateText("Password:", ResourceManager::Get().Fonts().Calibri(16), Color::Black, Transform({ -150, 32, 0 }), AlignH::Left, AlignV::Bottom);
+		passwordBox.SetObfuscating(true);
 
-		UIsurface& signInButton = passwordBox.Rectangle(280, 50, Color(50, 255, 50), Transform({ 0, -110, 0 }));
-		Text& signInText = signInButton.Text("Sign in", ResourceManager::Get().Fonts().Calibri(22), Color::Black, Transform({ 0, 0, 1 }), AlignH::Center);
-		UIsurface& registerButton = signInButton.Rectangle(280, 50, Color(50, 50, 255), Transform({ 0, -60, 0 }));
-		Text& registerText = registerButton.Text("Register", ResourceManager::Get().Fonts().Calibri(22), Color::Black, Transform({ 0, 0, 1 }), AlignH::Center);
+		UISurface& signInButton = passwordBox.CreateSurface(280, 50, Color(50, 255, 50), Transform({ 0, -110, 0 }));
+		UIText& signInText = signInButton.CreateText("Sign in", ResourceManager::Get().Fonts().Calibri(22), Color::Black, Transform({ 0, 0, 1 }), AlignH::Center);
+		UISurface& registerButton = signInButton.CreateSurface(280, 50, Color(50, 50, 255), Transform({ 0, -60, 0 }));
+		UIText& registerText = registerButton.CreateText("Register", ResourceManager::Get().Fonts().Calibri(22), Color::Black, Transform({ 0, 0, 1 }), AlignH::Center);
 
-		registerButton.EventHandler().OnClicked.AddEventListener([&registerScene](Event<UIClickedEvent>& e)
+		registerButton.Events().OnClick.AddEventListener([&registerScene](Event<UIClickedEvent>& e)
 			{
 				SceneManager::Get().SetCurrentScene(registerScene);
 				e.StopPropagation();
--- Fireblaze/src/Scenes/RegisterScene.cpp
+++ Fireblaze/src/Scenes/RegisterScene.cpp
@@ -6,24 +6,24 @@ namespace Fireblaze
 	void CreateRegisterScene(Scene& scene, int width, int height, Scene& loginScene)
 	{
 		Camera* registerCamera = scene.CreateCamera(Projection::Orthographic(0, width, 0, height, -100, 100));
-		Layer& registerLayer = scene.CreateLayer(registerCamera);
+		Layer& registerLayer = scene.CreateLayer(registerCamera, 100);
 
-		UIsurface& registerBackdrop = registerLayer.UI().Rectangle(400, 600, Color(200, 190, 200), Transform({ width / 2.0f, height / 2.0f, -90 }));
-		Text& title = registerBackdrop.Text("Fireblaze", ResourceManager::Get().Fonts().Verdana(48), Color::Black, Transform({ 0, 200, 1 }), AlignH::Center);
+		UISurface& registerBackdrop = registerLayer.UI().Root().CreateSurface(400, 600, Color(200, 190, 200), Transform({ width / 2.0f, height / 2.0f, -90 }));
+		UIText& title = registerBackdrop.CreateText("Fireblaze", ResourceManager::Get().Fonts().Verdana(48), Color::Black, Transform({ 0, 200, 1 }), AlignH::Center);
 
-		UIsurface& usernameBox = registerBackdrop.Rectangle(300, 50, Color(100, 100, 100), Transform({ 0, 100, 1 }));
-		Text& usernameText = usernameBox.Text("Username:", ResourceManager::Get().Fonts().Calibri(16), Color::Black, Transform({ -150, 32, 0 }), AlignH::Left, AlignV::Bottom);
-		UIsurface& passwordBox = usernameBox.Rectangle(300, 50, Color(100, 100, 100), Transform({ 0, -90, 0 }));
-		Text& passwordText = passwordBox.Text("Password:", ResourceManager::Get().Fonts().Calibri(16), Color::Black, Transform({ -150, 32, 0 }), AlignH::Left, AlignV::Bottom);
-		UIsurface& passwordConfirmBox = passwordBox.Rectangle(300, 50, Color(100, 100, 100), Transform({ 0, -80, 0 }));
-		Text& passwordConfirmText = passwordConfirmBox.Text("Confirm Password:", ResourceManager::Get().Fonts().Calibri(16), Color::Black, Transform({ -150, 32, 0 }), AlignH::Left, AlignV::Bottom);
-		UIsurface& emailBox = passwordConfirmBox.Rectangle(300, 50, Color(100, 100, 100), Transform({ 0, -90, 0 }));
-		Text& emailText = emailBox.Text("Email:", ResourceManager::Get().Fonts().Calibri(16), Color::Black, Transform({ -150, 32, 0 }), AlignH::Left, AlignV::Bottom);
+		UISurface& usernameBox = registerBackdrop.CreateSurface(300, 50, Color(100, 100, 100), Transform({ 0, 100, 1 }));
+		UIText& usernameText = usernameBox.CreateText("Username:", ResourceManager::Get().Fonts().Calibri(16), Color::Black, Transform({ -150, 32, 0 }), AlignH::Left, AlignV::Bottom);
+		UISurface& passwordBox = usernameBox.CreateSurface(300, 50, Color(100, 100, 100), Transform({ 0, -90, 0 }));
+		UIText& passwordText = passwordBox.CreateText("Password:", ResourceManager::Get().Fonts().Calibri(16), Color::Black, Transform({ -150, 32, 0 }), AlignH::Left, AlignV::Bottom);
+		UISurface& passwordConfirmBox = passwordBox.CreateSurface(300, 50, Color(100, 100, 100), Transform({ 0, -80, 0 }));
+		UIText& passwordConfirmText = passwordConfirmBox.CreateText("Confirm Password:", ResourceManager::Get().Fonts().Calibri(16), Color::Black, Transform({ -150, 32, 0 }), AlignH::Left, AlignV::Bottom);
+		UISurface& emailBox = passwordConfirmBox.CreateSurface(300, 50, Color(100, 100, 100), Transform({ 0, -90, 0 }));
+		UIText& emailText = emailBox.CreateText("Email:", ResourceManager::Get().Fonts().Calibri(16), Color::Black, Transform({ -150, 32, 0 }), AlignH::Left, AlignV::Bottom);
 
-		UIsurface& registerButton = emailBox.Rectangle(280, 50, Color(50, 50, 255), Transform({ 0, -75, 0 }));
-		Text& registerText = registerButton.Text("Register", ResourceManager::Get().Fonts().Calibri(22), Color::Black, Transform({ 0, 0, 1 }), AlignH::Center);
+		UISurface& registerButton = emailBox.CreateSurface(280, 50, Color(50, 50, 255), Transform({ 0, -75, 0 }));
+		UIText& registerText = registerButton.CreateText("Register", ResourceManager::Get().Fonts().Calibri(22), Color::Black, Transform({ 0, 0, 1 }), AlignH::Center);
 
-		registerButton.EventHandler().OnClicked.AddEventListener([&loginScene](Event<UIClickedEvent>& e)
+		registerButton.Events().OnClick.AddEventListener([&loginScene](Event<UIClickedEvent>& e)
 			{
 				SceneManager::Get().SetCurrentScene(loginScene);
 				e.StopPropagation();
