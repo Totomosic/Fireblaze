c21ce32b9c4143012590e230ab9595d9f397e335
Jordan Morrison <jordan.thomas.morrison@gmail.com>
2019-11-30 10:16:36 +0800

    Updated bolt

--- premake5.lua
+++ premake5.lua
@@ -9,24 +9,9 @@ workspace "Fireblaze"
     }
 
 -- Path to directory containing Bolt-Core
-InstallDir = "Bolt/"
+BoltDir = "Bolt/"
 
-include (InstallDir .. "Paths.lua")
-
-group("Resources")
-include (InstallDir .. ResourcesDir .. "Converter")
-group ("Bolt/Vendor")
-include (InstallDir .. BoltDir .. "vendor/GLFW")
-include (InstallDir .. BoltDir .. "vendor/Glad")
-include (InstallDir .. BoltDir .. "vendor/ImGui")
-include (InstallDir .. BoltDir .. "vendor/spdlog")
-include (InstallDir .. BoltDir .. "vendor/FreeType")
-include (InstallDir .. BoltDir .. "vendor/FreeType-GL")
-include (InstallDir .. BoltDir .. "vendor/Lua")
-group("Bolt/Vendor/Python")
-include (InstallDir .. BoltDir .. "vendor/Python/cpython/PCBuild")
-group ("Bolt")
-include (InstallDir .. BoltDir)
+include (BoltDir .. "BoltInclude.lua")
 
 IncludeDirs["FireblazeUtils"] = "Fireblaze-Utils/src"
 IncludeDirs["FireblazeLoginServer"] = "Fireblaze-LoginServer/src"
